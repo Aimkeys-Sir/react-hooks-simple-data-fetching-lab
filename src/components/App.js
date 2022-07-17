77f456faf04580dfe25666b926bd1d343e446e21
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-17 17:59:03 +0300

    commit tree 8ea132568f4ddae65a0b7af743c45a279f454e86

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,15 @@
 // create your App component here
+import React, { useEffect, useState } from "react"
+
+export default function App() {
+    const [dog, setDogs] = useState(null)
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(res => {
+                setDogs(res.message)
+            })
+    }, [])
+    if (!dog) return <p>Loading...</p>
+    return <img src={dog} alt="A Random Dog" />
+}
